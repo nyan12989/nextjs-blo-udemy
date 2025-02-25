faa3c403b7b9f05370d3841f1711fa2396f9e97f
nyan12989 <nyanboul@gmail.com>
2025-02-25 11:37:46 +0900

    最新の修正を反映

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/home.module.css";
 import Head from "next/head";
 import Layout from "@/cmp/layout";
 import Link from "next/link";
