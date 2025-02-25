1b3aa677f56639e3962baef190afe5a373d9b1a1
nyan12989 <nyanboul@gmail.com>
2025-02-25 11:48:10 +0900

    最新の修正を反映

--- pages/posts/post.tsx
+++ pages/posts/post.tsx
@@ -1,18 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "@/cmp/layout";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { remark } from "remark";
-import html from "remark-html"
 
-
-
-
-const postsDirectory=path.join(process.cwd(),"posts");
-
-
+// Nodeモジュールのインポートを削除
 
 export default function Post() {
   return (
@@ -29,34 +19,5 @@ export default function Post() {
   );
 }
 
-
-export function getStaticids() {
-  const filenames=fs.readdirSync(postsDirectory);
-
-  return filenames.map((filename) => {
-      return {params:{id:filename.replace(".md","")}}
-  });
-}
-
-
-export async function getPostData(id:string)
-{
-  const fullPath=path.join(postsDirectory,`${id}.md`);
-  const fileContents=fs.readFileSync(fullPath,"utf8");
-
-  const matterResult=matter(fileContents);
-
-  const blogContent=await remark()
-  .use(html)
-  .process(matterResult.content);
-
-  const blogContentHtml=blogContent.toString();
-
-
-  return {
-    id,
-    blogContentHtml,
-    ...matterResult.data,
-    
-  }
-}
+// getStaticidsとgetPostData関数を削除
+// これらはlib/post.tsに既にあるはずです
